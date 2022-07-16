e5c1f5001ab2cc4cd774c0f5bc474bfd4b01cbf6
Victor Emanuel Ferreira <victoremanueldsf@gmail.com>
2022-07-16 11:28:03 -0300

    Foi iniciado a adição a funcionalidade do popup para adicionar tarefas. É necessário corrigi-la e atualiza-la

--- script.js
+++ script.js
@@ -2,6 +2,58 @@ var botao_func = document.getElementById("botao_funcionalidade")
 
 botao_func.addEventListener("click", abrir_popup)
 
+function abrir_popup(){
+
+    let form = document.getElementById("form_func")
+
+    form.style.display = "inline"
+
+    console.log("teste")
+
+}
+
+var img_func = document.getElementById("img_func")
+
+console.log(img_func)
+
+img_func.addEventListener("click", fechar_img)
+
+
+function fechar_img(){
+    let form = document.getElementById("form_func")
+
+    form.style.display = "none"
+
+    console.log("teste")
+}
+
+var i = 0   
+
+var tarefas = document.getElementById("botao_adicionar")
+
+tarefas.addEventListener("click", adicionar_tarefas)
+
+function adicionar_tarefas(){
+
+    let tarefas = []
+
+    let adicionando= []
+
+    tarefas[i] = document.getElementById("tarefas").value
+
+    adicionando[i] = document.getElementById("novas_tarefas")
+
+    console.log(tarefas)
+
+    console.log(adicionando)
+
+    adicionando[i].innerHTML = "<li>" + tarefas + "</li>"
+    
+
+    i+=1
+}
 
 
 console.log(botao_func)
+
+
