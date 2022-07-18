52e9e80abe9346f738cebdfac18b5d489a7cd303
Victor Emanuel Ferreira <victoremanueldsf@gmail.com>
2022-07-17 23:17:06 -0300

    Foi adicionado sistema para guardar os itens utilizando o Localstorage. Foi iniciado uma melhoria na reponsividade e no design da página

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 var botao_func = document.getElementById("botao_funcionalidade")
 
 botao_func.addEventListener("click", abrir_popup)
@@ -38,7 +39,7 @@ function adicionar_tarefas(){
 
     let adicionando
 
-    tarefas = document.getElementById("tarefas").value
+    criar = document.getElementById("tarefas").value
 
     lu= document.getElementById("novas_tarefas")
 
@@ -48,15 +49,68 @@ function adicionar_tarefas(){
 
     adicionando = document.createElement('li');
 
-    adicionando.innerHTML = tarefas + "<hr>" 
+    adicionando.innerHTML = criar + "<hr>" 
 
     lu.appendChild(adicionando)
 
-    console.log(adicionando)
+    guarda_valor(criar)
 
 }
 
 
-console.log(botao_func)
+
+
+
+if (localStorage.hasOwnProperty("lista")) {
+    var guarda_valores = JSON.parse(localStorage.getItem("lista"))
+  }
+else{
+     guarda_valores = {tarefas_guard: []}
+}
+
+
+
+function guarda_valor(criar)
+{
+
+    guarda_valores.tarefas_guard.push(criar)
+    
+    json = JSON.stringify(guarda_valores)
+
+    localStorage.setItem("lista", json)
+
+    console.log(json)
+    console.log(criar)
+    console.log(guarda_valores.tarefas_guard)
+
+    
+
+}
+
+
+function recupera_dados()
+{
+    console.log("teste 2")
+    let mudar_json = localStorage.getItem("lista")
+
+     let novo_json = JSON.parse(mudar_json)
+
+    for (let i in novo_json.tarefas_guard)
+    {
+        let lu = document.getElementById("novas_tarefas")
+
+        let adicionando = document.createElement('li');
+
+        adicionando.innerHTML = novo_json.tarefas_guard[i] + "<hr>" 
+    
+        lu.appendChild(adicionando)
+
+        console.log(novo_json)
+        console.log(i)
+    } 
+}
+
+recupera_dados()
+
 
 
